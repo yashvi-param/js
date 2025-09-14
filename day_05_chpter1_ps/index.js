ec4c4f1618f6a18addbf0d2418ec0927c3d81910
yashvi-param <yashvirwn123@gmail.com>
2025-09-14 13:20:30 +0530

    Add files via upload
    complete

---
+++ day_05_chpter1_ps/index.js
@@ -0,0 +1,31 @@
+//chapter 1 = question 1
+let a = "yashvi"
+
+let b = 6
+console.log(a + b)
+
+//chapter 1 = question 2
+console.log(typeof (a + b))
+
+//chapter 1 = question 3
+const a1 = {
+    name: "yashvi",
+    section: 1,
+    isPrincipal: false
+}
+// a1 = 45
+//chapter 1 = question 4
+ a1 ['friend'] = "mahi" 
+ a1 ['name'] = "lovish" 
+ console.log(a1)
+
+ const dict = {
+    appearance:"an occasion when someone appears in public",
+    biography:"a written account of someone's life",
+    celebrity:"a famous person",
+    diligent:"someone who works hard and is careful and thorough",
+    ecstasy:"extreme happiness"
+}
+
+console.log(dict.biography)
+// also exsece with['']
