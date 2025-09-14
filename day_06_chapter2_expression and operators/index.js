3e093cc95b4c2295013a11d0055cc2db77e79549
yashvi-param <yashvirwn123@gmail.com>
2025-09-14 13:20:59 +0530

    Add files via upload
    complete

---
+++ day_06_chapter2_expression and operators/index.js
@@ -0,0 +1,48 @@
+77;
+"yashvi"
+
+console.log("operators in js");
+//arithmetic operators
+let a = 45;
+let b = 4; 
+
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a ** b = ", a ** b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("++a = ", ++a);
+// console.log("a++ = ", a++);
+// console.log("--a = ", --a);
+// console.log("a-- = ", a--);
+// console.log("a = ", a);
+// console.log("a-- = ", a--);
+
+//assignment operators
+let assignment = 1;
+ assignment += 8; //same as assignment = a + 8
+    console.log(assignment);
+assignment -= 8; //same as assignment = a - 8
+    console.log(assignment);
+assignment *= 8;    //same as assignment = a * 8
+    console.log(assignment);
+assignment /= 8;    //same as assignment = a / 8        
+    console.log(assignment);
+
+
+//comparison operators
+let comp1 = 6;
+let comp2 = 6;
+console.log("comp1 == comp2" , comp1 == comp2);
+console.log("comp1 != comp2" , comp1 != comp2);  
+console.log("comp1 === comp2" , comp1 === comp2);
+console.log("comp1 !== comp2" , comp1 !== comp2);
+console.log("comp1 > comp2" , comp1 > comp2);
+
+//logical operators
+let x = 5;
+let y = 10;
+console.log(x<y && x==5); 
+console.log(x>y || x==5);
+console.log(!false);
+console.log(!true);
